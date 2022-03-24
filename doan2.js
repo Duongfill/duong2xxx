0c4bd694c0050769212407c94affd5da998c5908
Duongfill <duong2962002@gmail.com>
2022-03-24 17:51:05 +0700

    duongdeptrai

---
+++ doan2.js
@@ -0,0 +1,43 @@
+// const adressbtn = document.querySelector('#add-form')
+// adressbtn.addEventListener("click",function(){
+//     document.querySelector('.add-form').style.display ="flex"
+// })
+// const adressclose = document.querySelector('#add-close')
+// adressclose.addEventListener("click",function(){
+//     document.querySelector('.add-form').style.display ="none"
+// })
+//slide
+// const imgNuber = document.querySelectorAll('.slides .slide img')
+// // console.log(imgNuber.length)
+// let index = 0
+// function imgauto(){
+//     index = index + 1
+//     if(index > imgNuber.length-1){
+//         index = 0
+//     }
+//     document.querySelector(".slides .slide").style.right = index * 100 +"%" 
+//     console.log(index)
+// }
+// setInterval(imgauto,300)
+/*-----*/
+// console.log(123)
+// var lbx = document.querySelector('.fa-chevron-circle-left-two')
+
+// var nbx = document.querySelector('.fa-chevron-circle-left-two')
+// const imgNuber = document.querySelectorAll('.slider-product-one-content-items')
+// console.log(lbx)
+// console.log(nbx)
+// rightbuton.addEventListener("click",function(){
+//     index = index +1
+//     if(index > imgNuber.length - 1){
+//         index = 0
+//     }
+//     document.querySelectorAll(".slider-product-one-content-items-content").style.right = index * 100 + "%"
+// })
+// leftbuton.addEventListener("click",function(){
+//     index = index -1
+//     if(index > imgNuber.length - 1){
+//         index = 0
+//     }
+//     document.querySelectorAll(".slider-product-one-content-items-content").style.right = index * 100 + "%"
+// })
